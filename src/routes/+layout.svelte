800f0bb86451896754aaa91bcb5e5c7021f94a7e
naturesh <yangtaehwan@yangtaehwan-ui-MacBookPro.local>
2023-11-19 00:14:11 +0900

    commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+</script>
+
+
+<div class="flex justify-center items-center w-screen h-screen">
+    <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,357 @@
+<script lang="ts">
+
+    const dict : any = {
+        lesson5: `She was awakened by the sound of her mother’s voice.
+그녀는 엄마의 목소리에 의해 깨어났다.
+The burglar ran away when the alarm went off.
+경보가 울렸을 때, 강도가 도망갔다.
+You can adjust the volume by turning the dial.
+다이얼을 돌려 볼륨을 조정할 수 있다.
+She loves the aroma of freshly brewed coffee.
+그녀는 신선하게 끓여진 커피 향을 좋아한다.
+The client acted on the advice of his lawyers.
+그 의뢰인은 그의 변호사의 의견에 따라 행동했다.
+This electronic dictionary is small and convenient to use.
+이 전자 사전은 작고 사용하기에 편하다.
+Is the new packaging environmentally friendly?
+새 포장재가 환경친화적인가요?
+He was accused of manipulating the price of gold.
+그는 금 가격을 조작하여 고소당했다.
+The situation became worse with the third-party intervention.
+제 3자의 개입으로 상황이 악화되었다.
+TV ads boost customers’ purchasing power.
+TV 광고들은 소비자의 구매력을 신장시킨다.
+The new equipment will give us increased operational efficiency.
+새 장비는 우리에게 증가된 운영적 효율성을 줄 것입니다.
+The construction took seven years and cost 2.5 trillion won.
+그 건설 공사는 7년이 걸렸고 2조 5천억의 비용이 들었다.
+Each conference room is equipped with a projector and screen.
+각각의 회의실은 프로젝트와 스크린을 갖추고 있다.
+This program helps you manage your device remotely.
+이 프로그램은 당신이 원격으로 당신의 장치를 관리하는데 도움을 준다.
+He installed the air conditioner by himself.
+그는 혼자서 에어컨을 설치했다.
+The atmosphere helps regulate the temperature of the earth.
+대기는 지구의 온도를 조절하는데 도움을 준다.
+We ran out of toilet paper.
+우리는 화장지가 다 떨어졌다.
+One keeps track of how much time has passed.
+시간이 얼마나 흘렀는지에 대해 파악하고 있다.
+This soy milk has passed its expiration date.
+이 두유는 유효 기간이 지났다.
+I think this fish has spoiled because it smells bad.
+이 생선은 나쁜 냄새가 나기 때문에 상했다고 생각합니다.
+The moisture moves toward the roots.
+습기가 뿌리 쪽으로 향해 움직인다.
+The jewelry shop has a security detection system.
+보석 상점에는 보안 탐지 체계가 있다.
+The boat has a leak and is filling up with water.
+배에 누수가 생겨 물이 차오르고 있다.
+It seems that the kitchen drain is blocked.
+부엌 배수관이 막힌 것 같다.
+Notify me immediately if you are in trouble.
+만일 당신이 곤경에 처하게 되면 나에게 즉시 알려주세요.
+The security of the building is tight.
+그 건물의 보안은 철저하다. 
+The break-in occurred in the middle of the night.
+한밤중에 침입이 일어났다.
+She's reading a book about fitness and eating habits.
+그녀는 건강과 식습관에 대한 책을 읽고 있다.
+A fitness tracker is a good way to monitor your activity level and health.
+건강 추적기는 당신의 활동 수준과 건강을 모니터하는 좋은 방법이다.
+The doctor checked his patient's vital signs.
+그 의사는 환자의 활력 징후를 확인했다.
+Diet pills can cause side effects such as increased heart rate, sweating, insomnia, etc.
+다이어트 알약은 증가된 심장 박동수, 발한, 불면증 등과 같은 부작용을 일으킬 수 있다.
+I came down with the flu a few days ago.
+나는 몇일 전 독감에 걸렸다.
+She had a diabetic reaction but is okay now.
+그녀는 당뇨 반응을 보였으나 지금은 괜찮다.
+Those wooden chopsticks and food containers are disposable.
+나무 젓가락과 음식 용기는 일회용이다.
+Eating too much food may cause your blood sugar level to increase.
+과식은 당신의 혈당 수치가 증가하게 되는 원인이 될 수 있다.
+Radio waves are transmitted through the air.
+무선 전파가 공기를 통해 전송된다.
+She suffers from diabetes and has to use insulin.
+그녀는 당뇨병을 앓고 있어서 인슐린을 사용해야 한다.
+The drug is only effective when it is paired with a regular exercise routine.
+그 약은 정기적인 운동 일정과 병행될 때에 만 효력이 있다.
+We’re planning to implement a new advertising strategy.
+우리는 새 광고 전략을 시행할 계획 중이다.
+I merely asked what I want to know.
+나는 단지 내가 알고 싶은 것을 물었다.
+She’s going to launch a small business soon.
+그녀는 작은 사업을 곧 시작할 예정이다.
+This machinery has the capability of lifting approximately two tons.
+이 기계는 대략 2톤을 들어 올릴 수 있는 성능을 가지고 있다.
+The meeting took place at a hotel.
+그 모임은 호텔에서 열렸다.
+You must keep a positive and optimistic attitude to succeed.
+당신은 성공하기 위해서 긍정적이고 낙천적인 태도를 가져야 한다.
+Interconnection with people enables you to reach your full potential.
+사람과의 상호 연관성은 당신이 잠재력을 최대한 발휘할 수 있게 한다.
+The father of the girl brushed aside his daughter's complaints.
+그 소녀의 아버지는 딸의 불평을 무시했다.
+By abusing his power, the manager angered the employees.
+그의 힘을 남용함으로써, 그 매니저는 피고용인들을 화나게 했다.
+`,
+        lesson4: `A caterpillar turns into a butterfly.
+애벌레가 나비로 변한다.
+In many species of butterflies, the change from pupa to adult takes about two weeks.
+많은 종의 나비들은 번데기에서 성충으로 변화하는데 약 2주가 걸린다.
+He committed a lot of evil crimes.
+그는 많은 사악한 범죄를 저질렀다.
+She stands out among the new recruits.
+그녀는 새로 선발된 부원 중에서 두드러진다.
+It is those pioneers who have paved the way for us to follow.
+우리가 따르는 길을 닦았던 사람들은 바로 선구자들이다.
+If you are like most people, you should notice something odd.
+만약 당신이 대부분의 사람들과 같다면, 당신은 이상한 것을 알아채야 한다.
+His behavior was a bit weird today.
+오늘 그의 행동은 약간 기이했다.
+Do you have permission to enter?
+당신은 입장 허락을 받았나요?
+The shipping firm provided compensation for the damaged camera.
+그 운송 회사는 파손된 카메라에 대한 보상금을 제공했다.
+She has a keen interest in learning new languages.
+그녀는 새 언어를 배우는데 강한 흥미를 가지고 있다.
+He was obsessed by his fear of the operation.
+그는 수술 공포증에 사로잡혀 있다. The traffic continued on for miles on end.
+교통량이 수 마일을 계속해서 이어졌다.
+The artist depicted children playing at a nursery.
+그 예술가는 유치원에서 놀고 있는 아이들을 그렸다.
+There are subtle differences between the two paintings.
+그 두 그림 사이에는 미묘한 차이들이 있다.
+When we closed the door, the noise died down.
+우리가 문을 닫았을 때, 소음이 차츰 잦아들었다.
+The boy was still awake when his mom came home.
+그의 엄마가 집에 왔을 때, 그 소년은 여전히 깨어 있었다.
+It took a few months to adjust to the new surroundings.
+새 환경에 적응하는데 몇달이 걸렸다.
+She has a collection of rare insect specimens.
+그녀는 희귀한 곤충 표본 수집품을 소유하고 있다.
+They sell a selection of plain and patterned ties.
+그들은 무늬가 없는 타이와 무늬가 있는 타이를 판다.
+Music for motion pictures often serves to authenticate the era.
+영화 음악은 종종 시대를 입증하는 데 도움이 된다.
+The house was filled with marvelous wooden floors that had to be waxed regularly.
+그 집은 정기적으로 왁스 칠이 되어야 하는 신기한 나무 바닥으로 가득 차 있었다.
+The restaurant has an exotic atmosphere.
+그 식당은 이국적인 분위기이다.
+The animal's lifeless body lay on the floor.
+그 동물의 시체가 바닥에 놓여 있었다.
+The nurse stuck a needle in my arm.
+그 간호사가 내 팔에 바늘을 찔렀다.
+The ship was headed for the harbor.
+그 배는 그 항구를 향하고 있었다.
+Everyone knows that Dokdo is Korean territory.
+모두가 독도는 한국 영토라는 것을 안다.
+She was bold enough to admit her mistakes.
+그녀는 자신의 실수를 인정할 정도로 대담하다.
+Her passion led her to become the greatest naturalist.
+그녀의 열정으로 그녀는 가장 위대한 자연학자가 되었다.
+The female entrepreneur took bold steps in a male-dominated world.
+그 여성 기업가는 남성 주도형의 세상에서 과감한 행보를 취했다.
+They set out on a journey to an unknown place.
+그들은 미지의 장소로 여행을 떠났다.
+She successfully led an expedition to the North Pole.
+그녀는 북극으로 가는 탐험을 성공적으로 이끌었다.
+Some plants are planted purely for decoration.
+어떤 식물은 오직 장식을 위해 심어진다.
+Competition for the job was intense.
+그 직업에 대한 경쟁이 극심했다.
+This dog is very fierce, so stay away from it.
+이 개는 매우 맹렬하다, 그래서 떨어져 있어야 한다.
+This flesh-eating insect lives in warm areas of the world.
+이 육식성 곤충은 따뜻한 지역에서 산다.
+Many who have experienced a major loss often go on to achieve remarkable feats in spite of their hardships.
+큰 손실을 경험했던 많은 사람들은 고난에도 불구하고 종종 주목할 만한 업적을 이룬다.
+A snail is creeping on the glass of a window.
+달팽이가 유리창 위로 기어가고 있다.
+The stem of a plant supports its flowers and leaves.
+식물의 줄기는 꽃과 나뭇잎을 지탱한다.
+He made a tremendous amount of money through his online shopping mall.
+그는 온라인 쇼핑 몰을 통해 엄청난 액수의 돈을 벌었다.
+She couldn't understand the classification system used in a library.
+그녀는 도서관에서 사용된 분류 체계를 이해할 수 없었다.
+Venice is celebrated for its beautiful canals.
+베니스는 아름다운 운하로 유명하다.
+This fabric is woven by hand.
+이 천은 손으로 엮은 것이다.
+The idea that the earth is flat prevailed a long time ago.
+지구가 평평하다는 생각은 오래전에 유행했다.
+The employee's performance will be reevaluated after six months.
+그 고용인들의 성과가 6달 후에 다시 평가 되어질 것이다.
+I recognized her face but not her name.
+나는 그녀의 이름이 아니라 얼굴을 알아보지 못했다.
+She has an extraordinary power to solve riddles.
+그녀는 수수께끼를 해결하는 뛰어난 능력을 가지고 있다.
+It’s an accomplishment you can be proud of.
+그것은 당신이 자랑스러워할 수 있는 업적이다.
+She was named after her grandmother.
+그녀는 할머니의 이름을 따서 명명되었다.
+Participants will be given a certificate to demonstrate that they have completed the program.
+참석자들에게 그들이 그 프로그램을 완수했다는 것을 입증하는 증명서가 주어질 것이다.
+Creative ideas are usually viewed with suspicion and distrust.
+독창적인 생각들은 대개 의심과 불신을 가지고 판단되어 진다.
+I object to being an object of ridicule.
+나는 조롱의 대상이 되는 것에 반대한다.
+Rain is typical British weather.
+비는 일반적인 영국 날씨이다.
+`,
+        review: ``
+    }
+
+    var index : number = -1
+
+
+    var selected : string = ''
+    var dictionary : { ko : string, en : string }[] = []
+
+    async function loadDict(x : string) {
+
+        if(x == '' ) return
+
+        dictionary = []
+        const raw = (dict[x] as string).split('\n')
+
+
+        for(let i=0; i<raw.length; i+=2) {
+            dictionary.push({
+                ko : raw[i+1],
+                en : raw[i]
+            })
+        }
+
+        console.debug('Load complete!!  data from ' + x)
+        console.debug('Start session...')
+
+        index = -1
+        index = 0
+    }
+
+    
+
+    var ko = ''
+    var en : { d : string | undefined , b : boolean, e : boolean }[] = []
+
+    async function processData(index : number) {
+
+        if(index == -1) return
+        else if (dictionary.length == 0) return
+
+        const data = dictionary[index]
+        
+        ko = data.ko
+
+        const rawen = data.en.split(' ')
+       
+        var created = []
+
+        for(let i of rawen) {
+            if(Math.random() > 1 - 0.9) {
+
+                created.push({
+                    d : i.at(0),
+                    b : false,
+                    e : false
+                })
+                created.push({
+                    d : i.slice(1, i.length) ,
+                    b : true,
+                    e : true
+                })
+
+            }else {
+
+                created.push({
+                    d : i,
+                    b : false,
+                    e : true
+                })
+
+            }
+
+            
+
+        }
+
+        en = created
+       
+
+    }
+
+    $ : processData(index)
+
+</script>
+
+
+
+
+
+<div class="font-bold text-xl fixed top-7 left-7 h-fit">
+
+    <div>
+       easy.
+       <span class="text-xs font-thin">
+            <span class="ml-1 mr-1">[</span> 
+            english
+            <span class="ml-1 mr-1">]</span>
+       </span>
+       <div class="text-xs font-thin"> owner & developer : github-naturesh, 양태환 </div>
+    </div>
+
+    <div class="text-xs mt-5 flex gap-3 font-light">
+        <button class="shadow-xl p-3 rounded-xl hover:scale-110 duration-200" on:click={ async () => loadDict('lesson4')}>L 4</button>
+        <button class="shadow-xl p-3 rounded-xl hover:scale-110 duration-200" on:click={ async () => loadDict('lesson5')}>L 5</button>
+        <button class="shadow-xl p-3 rounded-xl hover:scale-110 duration-200" on:click={ async () => loadDict('review')}>R E : {(dict.review.split('\n').length-1)/2}</button>
+    </div>
+    
+</div>
+
+<div class="flex flex-col justify-center items-center w-10/12">
+
+
+    <!-- Question Area -->
+    {#key index}
+        <div class="font-mono text-lg leading-loose w-full">
+            {#if en.length}
+                {#each en as e}
+                    <button class={e.b ? 'blur-sm' : '' + 'inline-block duration-300 border-none'} on:click={ async () => { e.b = !e.b}}>
+                        <span class={e.e ? 'mr-2' : ''}>
+                            {e.d}
+                        </span>
+                    </button>
+                {/each}
+            {:else}
+                Welcome to Easy. for english
+            {/if}
+            
+        </div>
+        <div class="font-mono text-sm leading-loose mt-16 shadow-sm rounded-xl p-3 w-full">
+            { ko || '영단어 암기, 복습, 문장암기'}
+        </div>
+    {/key}
+
+    <div class="mt-5 w-full fixed bottom-7">
+        <div class="w-5/6 flex flex-row m-auto">
+            <div class="w-1/2">
+                <button class="h-14 w-5/6 m-auto block rounded-xl hover:scale-110 duration-200 bg-slate-50 text-slate-500 text-xs animate-bounce" on:click={ async () => index += 1}>Next</button>
+            </div>
+            <div class="w-1/2">
+                <button class="h-14 w-5/6 m-auto block rounded-xl hover:scale-110 duration-200 bg-slate-50 text-slate-500 text-xs" on:click={ async () =>{
+                    const data = dictionary[index]
+
+                    if(data) {
+                        dict.review += data.en + '\n' + data.ko + '\n'
+                        
+                    }
+
+                    index += 1
+
+                }}>X</button>
+            </div>
+        </div>
+    </div>
+</div>
